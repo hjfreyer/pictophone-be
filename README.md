d7af15e95c272fbe4d18da43a0f4c4b9710b207c
Hunter Freyer <yt@hjfreyer.com>
2019-12-04 21:39:04 -0500

    Split the backend into its own repo

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Pictophone
+
+The backend. Frontend is available at https://github.com/hjfreyer/pictophone.
--- fe/src/base.ts
+++
@@ -1,9 +0,0 @@
-import Action from "./model/Action"
-import { Upload, UploadResponse } from "./model/rpc"
-
-export type Dispatch = {
-    action(a: Action): Promise<void>
-    upload(u: Upload): Promise<UploadResponse>
-}
-
-export const MODEL_VERSION = 'v1.2.0'
